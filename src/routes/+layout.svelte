a62521800dadce1f9024d2ec2ec54a5a474e6956
jandriw <jan.barcos@gmail.com>
2024-02-16 19:53:46 +0100

    layout modifications (backgrownd and font)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
     import "../app.css";
+    import '@fontsource/rajdhani'
 </script>
   
 <slot />
+
+<style lang="postcss">
+    :global(html) {
+      background-color: #11111f;
+      font-family: 'Rajdhani', sans-serif;
+    }
+</style>
