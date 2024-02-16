039950c57d55f2ca768ab8fe542845fe87970458
jandriw <jan.barcos@gmail.com>
2024-02-16 20:14:41 +0100

    feat: vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
     import "../app.css";
     import '@fontsource/rajdhani'
     import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
 
     injectSpeedInsights();
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
   
 <slot />
